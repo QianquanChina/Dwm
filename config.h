dbbb9175caa27fe27f2de0ef193fe8368ad66043
Jc <370047797@qq.com>
2021-12-21 20:41:39 +0800

    change fonts size

--- config.h
+++ config.h
@@ -14,8 +14,8 @@ static const int sidepad            = 10;       /* horizontal padding of bar */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "CodeNewRoman Nerd Font Mono:size=16" };
-static const char dmenufont[]       = "CodeNewRoman Nerd Font Mono:size=16";
+static const char *fonts[]          = { "CodeNewRoman Nerd Font Mono:size=15" };
+static const char dmenufont[]       = "CodeNewRoman Nerd Font Mono:size=15";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -63,7 +63,7 @@ static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ ""  ,      tile },    /* first entry is default */
 	{ "缾" ,      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "",      monocle },
 };
 
 /* key definitions */
@@ -76,10 +76,9 @@ static const Layout layouts[] = {
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
-
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "rofi", "-no-lazy-grab", "-show", "drun", "-modi", "drun", "-theme", "~/.config/rofi/launchers/misc/blurry", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
