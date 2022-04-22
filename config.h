6876ad777d309d513c22b7f9a63639b738574f4f
jc <370047797@qq.com>
2022-04-22 14:52:37 +0800

    for statusbar colors

--- config.h
+++ config.h
@@ -1,5 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 /* appearance */
+static const unsigned int statusbar = 5;        /* 修改状态栏的宽度 */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
@@ -17,6 +18,8 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
+static const char col_white[]       = "#ffffff";
+static const char col_black[]       = "#000000";
 
 static const char col_purp1[]       = "#1d1f21";
 static const char col_cyan[]        = "#005577";
@@ -28,8 +31,9 @@ static const char *colors[][3]      = {
 	//[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	//[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 
-	[SchemeNorm] = { col_gray3, col_gray2, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_purp1,  col_cyan  },
+	[SchemeNorm] = { col_white, col_gray2,  col_gray2 },
+	[SchemeSel]  = { col_black, col_gray2,  col_cyan  },
+//	[SchemeSel]  = { col_gray4, col_purp1,  col_cyan  },
 };
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
